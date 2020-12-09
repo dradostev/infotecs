0a875970df868b3e9c772cf96fa5aa0cdf58088b
Daniil Radostev <Daniil.Radostev@infotecs.ru>
2020-12-09 15:59:55 +0300

    article service

--- Source/Infotecs.Articles.Server.Application/Services/ArticlesService.cs
+++ Source/Infotecs.Articles.Server.Application/Services/ArticlesService.cs
@@ -1,7 +1,33 @@
-﻿namespace Infotecs.Articles.Server.Application.Services
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace Infotecs.Articles.Server.Application.Services
 {
     public class ArticlesService : Articles.ArticlesBase
     {
-        
+        public override Task<Article> CreateArticle(CreateArticleRequest request, ServerCallContext context)
+        {
+            return base.CreateArticle(request, context);
+        }
+
+        public override Task<ListArticlesReply> ListArticles(EmptyRequest request, ServerCallContext context)
+        {
+            return base.ListArticles(request, context);
+        }
+
+        public override Task<ShowArticleReply> ShowArticle(ShowArticleRequest request, ServerCallContext context)
+        {
+            return base.ShowArticle(request, context);
+        }
+
+        public override Task<EmptyReply> DeleteArticle(DeleteArticleRequest request, ServerCallContext context)
+        {
+            return base.DeleteArticle(request, context);
+        }
+
+        public override Task<Comment> AddComment(AddCommentRequest request, ServerCallContext context)
+        {
+            return base.AddComment(request, context);
+        }
     }
 }
---
+++ Source/Infotecs.Articles.Server.Domain/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infotecs.Articles.Server.Domain.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<T> Show(long entityId);
+        Task<IEnumerable<T>> List();
+        Task<T> Create(T entity);
+        Task Delete(long entityId);
+    }
+}
