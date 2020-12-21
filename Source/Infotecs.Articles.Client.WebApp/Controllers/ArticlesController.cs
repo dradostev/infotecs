04d1745921842397d222b505ad5f75bb9ebf3d47
Daniil Radostev <Daniil.Radostev@infotecs.ru>
2020-12-21 20:12:25 +0300

    init angular web app

---
+++ Source/Infotecs.Articles.Client.WebApp/Controllers/ArticlesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Infotecs.Articles.Client.WebApp.Controllers
+{
+    [ApiController, Route("articles")]
+    public class ArticlesController : Controller
+    {
+        
+    }
+}
