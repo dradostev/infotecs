a579d770b809e852cc4467a4aa3642a7dad275a3
Daniil Radostev <Daniil.Radostev@infotecs.ru>
2020-12-29 15:46:21 +0300

    using fix

--- Source/Infotecs.Articles.Client.WebApp/Pages/Error.cshtml.cs
+++ Source/Infotecs.Articles.Client.WebApp/Pages/Error.cshtml.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace Infotecs.Articles.Client.WebApp.Pages
 {
@@ -13,15 +8,11 @@ namespace Infotecs.Articles.Client.WebApp.Pages
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
-        private readonly ILogger<ErrorModel> logger;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="ErrorModel"/> class.
         /// </summary>
-        /// <param name="logger">Logger injection.</param>
-        public ErrorModel(ILogger<ErrorModel> logger)
+        public ErrorModel()
         {
-            this.logger = logger;
         }
         
         /// <summary>
