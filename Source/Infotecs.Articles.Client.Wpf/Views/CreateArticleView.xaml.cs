67578b2f415c90262b64152a726f12836d0726d5
Daniil Radostev <Daniil.Radostev@infotecs.ru>
2020-12-30 12:05:26 +0300

    Web client branch

--- Source/Infotecs.Articles.Client.Wpf/Views/CreateArticleView.xaml.cs
+++ Source/Infotecs.Articles.Client.Wpf/Views/CreateArticleView.xaml.cs
@@ -2,8 +2,14 @@
 
 namespace Infotecs.Articles.Client.Wpf.Views
 {
+    /// <summary>
+    /// Create Article code behind.
+    /// </summary>
     public partial class CreateArticleView : UserControl
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateArticleView"/> class.
+        /// </summary>
         public CreateArticleView()
         {
             InitializeComponent();
